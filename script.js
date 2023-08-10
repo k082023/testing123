62cf3f366de85903643b992990a94cccabbaa249
k082023 <141926550+k082023@users.noreply.github.com>
2023-08-10 21:18:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// JavaScript for loading JSON data and creating cards
+// Function to fetch JSON data from the server
+async function fetchJsonData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching JSON data:', error);
+        return [];
+    }
+}
 
-// Function to create a card
-function createCard(data) {
-    const cardContainer = document.getElementById('cardContainer');
+// Function to create a card element
+function createCardElement(data) {
     const card = document.createElement('div');
     card.classList.add('card');
 
-    // Create card content (you can modify this based on your JSON structure)
+    // Create card content
     card.innerHTML = `
         <img src="${data.image}" alt="${data.title}">
         <h2>${data.title}</h2>
@@ -14,18 +25,31 @@ function createCard(data) {
         <!-- Add more content here based on your JSON structure -->
     `;
 
-    // Append the card to the container
-    cardContainer.appendChild(card);
+    return card;
 }
 
-// Fetch and load JSON data
-fetch('data.json')
-    .then(response => response.json())
+// Function to render cards
+function renderCards(data) {
+    const cardContainer = document.getElementById('cardContainer');
+    if (!cardContainer) {
+        console.error('Card container not found.');
+        return;
+    }
+
+    // Clear existing content
+    cardContainer.innerHTML = '';
+
+    // Create and append card elements
+    data.forEach(item => {
+        const cardElement = createCardElement(item);
+        cardContainer.appendChild(cardElement);
+    });
+}
+
+// Load and render cards from the JSON data file
+const jsonDataUrl = 'data.json';
+
+fetchJsonData(jsonDataUrl)
     .then(data => {
-        data.forEach(item => {
-            createCard(item);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+        renderCards(data);
     });
